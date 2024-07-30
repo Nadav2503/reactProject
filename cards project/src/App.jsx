8a217e00dc2497cb5e6271f9a40874b092c44d6a
Nadav2503 <nadavivon.re@gmail.com>
2024-07-30 09:54:36 +0300

    first commit

---
+++ cards project/src/App.jsx
@@ -0,0 +1,11 @@
+import Cards from "./cards/components/Cards";
+import GridExample from "./sandbox/GridExample"
+function App() {
+  const user = null;
+  return (
+    <div>
+      <Cards />
+    </div>)
+}
+
+export default App;
